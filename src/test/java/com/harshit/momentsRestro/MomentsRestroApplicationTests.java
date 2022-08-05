38fdc17ef7571455a9f80b26155541be9dc7075c
Harshit <tamrakarharshit@gmail.com>
2022-08-06 00:51:20 +0530

    inital commit

---
+++ src/test/java/com/harshit/momentsRestro/MomentsRestroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshit.momentsRestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentsRestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
